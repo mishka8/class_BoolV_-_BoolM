ac69f19c862f1820f48ea9f51ff4379e7ce4e4fe
mishka8 <144250192+mishka8@users.noreply.github.com>
2024-12-21 03:40:39 +0700

    Update class_BoolV.cpp

--- class_BoolV.cpp
+++ class_BoolV.cpp
@@ -1,394 +1,461 @@
+#include <cstddef>
 #include <iostream>
-#include <cstring>
+#include <istream>
+#include <ostream>
+#include <string.h>
+#include <string>
 #include <fstream>
-#include <vector>
 
+#include <vector>
 using namespace std;
 
-class BoolV
-{
-    unsigned int *vec;
-    int nbit;// nbit – количество бит вектора
-    int size_v;//размер вектора или количество элементов массива vec (bytes)
+
+class BoolVec {
+private:
+    unsigned int* vec; // Булев вектор
+    size_t bits;          // Кол-во бит вектора
+    size_t sizeVector;    // Кол-во эл-тов массива vector
 public:
-    BoolV(int nn = 1)
-    {
-        nbit = nn;
-        size_v = (nn - 1) / 32 + 1;
-        vec = new unsigned int[size_v];
-        for(int i = 0; i < size_v; i++)
-            vec[i] = 0;
+    BoolVec(size_t n = 1);
+    BoolVec(const char*);
+    BoolVec(const BoolVec&);
+    ~BoolVec();
+
+    void Set1(size_t);
+    void Set0(size_t);
+    string ConvertVecToStr() const;
+    BoolVec& operator= (const BoolVec&);
+    int operator[](size_t);
+
+    bool operator== (const BoolVec&);
+    BoolVec operator| (const BoolVec&);
+    BoolVec operator& (const BoolVec&);
+    BoolVec operator~ ();
+    int weight();
+
+    friend ostream& operator<< (ostream&, const BoolVec&);
+    friend istream& operator>> (istream&, BoolVec&);
+};
+
+
+string BoolVec::ConvertVecToStr() const 
+{
+    string stringVec = "";
+    unsigned int mask = 1;
+
+    if (vec == NULL || bits < 0)
+        return stringVec;
+
+    for (int i = 0; i < bits; i++) {
+        mask = 1;
+        if (vec[i / 32] & (mask << (i % 32)))
+            stringVec += '1';
+        else
+            stringVec += '0';
     }
 
-    BoolV(const char *str) : BoolV:: BoolV(strlen(str))
+    return stringVec;
+}
+
+
+BoolVec::BoolVec(size_t n) 
+{
+    if (!n) 
     {
-        unsigned int mask = 1;
-
-        for(int i = 0; i < nbit; i++)
-        {
-            if(mask == 0)
-                mask = 1;
-
-            if(str[i] != '0')
-                vec[i/32] = vec[i/32] | mask;
-
-            mask = mask << 1;
-
-
-        }
+        bits = 0;
+        sizeVector = 0;
+        vec = NULL;
+        return;
     }
 
-    BoolV(const BoolV &other)
+    int bytes = ((n - 1) / 8) + 1;
+    bits = n;
+    sizeVector = ((bytes - 1) / 4) + 1;
+    vec = new unsigned int[sizeVector];
+    for (int i = 0; i < sizeVector; i++)
+        vec[i] = 0;
+}
+
+
+BoolVec::BoolVec(const char* str) : BoolVec::BoolVec(strlen(str)) 
+{
+    unsigned int mask = 1;
+
+    for (int i = 0; i < bits; i++) 
     {
-        nbit = other.nbit;
-        size_v = other.size_v;
-        vec = new unsigned int[size_v];
-        for(int i = 0; i < size_v; i++)
-        {
-            vec[i] = other.vec[i];
-        }
+        if (str[i] != '0')
+            vec[i / 32] = vec[i / 32] | mask;
+
+        mask <<= 1;
+
+        if (mask == 0)
+            mask = 1;
     }
+}
 
-    ~BoolV()
-    {
-        if(vec != NULL)
-            delete[] vec;
 
-        nbit = 0;
-        size_v = 0;
-    }
+BoolVec::BoolVec(const BoolVec& obj) : BoolVec::BoolVec(obj.bits) 
+{
+    for (int i = 0; i < obj.sizeVector; i++)
+        vec[i] = obj.vec[i];
+}
 
-    int weight()
-    {
-        int count = 0;
-        unsigned mask = 1;
-        for(int i = 0; i < nbit; i++)
-        {
-            if(vec[i/32] & mask)
-                count++;
 
-            mask = mask << 1;
-
-            if(mask == 0)
-                mask = 1;
-        }
-        return count;
-    }
-
-    void set1(int pos)//5
-    {
-        if(pos > nbit || pos < 0)
-        {
-            cout << "Index out of range" << endl;
-            exit(-1);
-        }
-
-        unsigned int mask = 1;
-        int byte_v = pos / 32;
-        mask = mask << (pos % 32);
-        vec[byte_v] = vec[byte_v] | mask;
-    }
-
-    void set0(int pos)
-    {
-        if(pos > nbit || pos < 0)
-        {
-            cout << "INdex out of range" << endl;
-            exit(-2);
-        }
-
-        unsigned int mask = 1;
-        int byte_v = pos / 32;
-        mask = mask << (pos % 32);
-        vec[byte_v] = vec[byte_v] & ~(mask);
-    }
-
-    int operator[](int pos)
-    {
-        if(pos > nbit || pos < 0)
-        {
-            cout << "Index out of range" << endl;
-            exit(-3);
-        }
-
-        unsigned int mask = 1;
-        int byte_v = pos / 32;
-        mask = mask << (pos % 32);
-        bool res = vec[byte_v] & mask;
-
-        return res;
-    }
-
-    BoolV operator= (const BoolV &other)
-    {
-        if(this == &other)
-            return *this;
-
+BoolVec::~BoolVec() 
+{
+    if (vec != NULL)
         delete[] vec;
 
-        vec = new unsigned int[other.size_v];
-        nbit = other.nbit;
-        size_v = other.size_v;
-        for(int i = 0; i < size_v; i++)
-        {
-            vec[i] = other.vec[i];
-        }
+    bits = 0;
+    sizeVector = 0;
+}
 
+
+BoolVec& BoolVec::operator= (const BoolVec& obj) 
+{
+    if (this == &obj)
         return *this;
+
+    delete[] vec;
+
+    vec = new unsigned int[obj.sizeVector];
+    bits = obj.bits;
+    sizeVector = obj.sizeVector;
+
+    for (int i = 0; i < sizeVector; i++)
+        vec[i] = obj.vec[i];
+
+    return *this;
+}
+
+int BoolVec::operator[](size_t index) 
+{
+    if (index >= bits) 
+    {
+        cout << "Error []: index out of range!\n";
+        exit(1);
     }
 
-    bool operator==(const BoolV &other)
+
+    unsigned int mask = 1;
+    mask <<= (index % 32);
+
+    int r = bool(vec[index / 32] & mask);
+    
+    return r;
+}
+
+
+bool BoolVec::operator== (const BoolVec& obj) 
+{
+    if (obj.bits != bits)
+        return false;
+
+    for (int i = 0; i < sizeVector; i++) 
     {
-        if(nbit != other.nbit)
+        if (vec[i] != obj.vec[i])
             return false;
-        for(int i = 0; i < size_v; i++)
-        {
-            if(vec[i] != other.vec[i])
-                return false;
-        }
-
-        return true;
     }
 
-    BoolV operator|(const BoolV &other)//логическое или
-    {
-        int  size = nbit;
+    return true;
+}
 
-        BoolV res(size);
 
-        for(int i = 0; i < size_v; i++)
-        {
-            res.vec[i] = vec[i] | other.vec[i];
-        }
-        return res;
-    }
-
-    BoolV operator& (const BoolV &other)
-    {
-        int size = nbit;
-
-        BoolV res(size);
-
-        for(int i = 0; i < size_v; i++)
-        {
-            res.vec[i] = vec[i] & other.vec[i];
-        }
-        return res;
-    }
-
-    BoolV operator~()//????????
-    {
-        BoolV res(*this);//конструктор копирования
-
-        unsigned int mask = 65535;
-        for(int i = 0; i < size_v; i++)
-        {
-            res.vec[i] = ~vec[i];
-        }
-        mask = (1 << (nbit % 32)) - 1;
-        res.vec[size_v - 1] = res.vec[size_v - 1] & mask;
-
-        return res;
-    }
-
-    friend ostream& operator<< (ostream &os, const BoolV &boolV)
-    {
-        char *str = new char[boolV.nbit + 1];
-        str[boolV.nbit] = '\0';
-        for(int i = 0; i < boolV.nbit; i++)
-        {
-            unsigned int mask = 1;
-            if(boolV.vec[i/32] & (mask << (i % 32)))
-                str[i] = '1';
-            else
-                str[i] = '0';
-        }
-        os << str << endl;
-
-        return os;
-    }
-
-    friend istream& operator>> (istream &in, BoolV &boolV)
-    {
-        string str;
-        in >> str;
-
-        BoolV res(str.c_str());//Чтобы сделать указатель на строку в C++, можно использовать функцию-член c_str() для std::string.
-
-        boolV = res;
-
-        return in;
-    }
-
-};
-
-class BoolM
+void BoolVec::Set1(size_t bit) 
 {
-    BoolV *bm;
-    int m, n;// m – количество строк, n – количество столбцов
+    if (vec == NULL || bits < 0 || bits < bit)
+        return;
+
+    unsigned int mask = 1;
+    int byte = bit / 32;
+    int shiftBits = bit % 32;
+
+    mask = mask << shiftBits;
+    vec[byte] = vec[byte] | mask;
+}
+
+
+void BoolVec::Set0(size_t bit) 
+{
+    if (vec == NULL || bits < 0 || bits < bit)
+        return;
+
+    unsigned int mask = 1;
+    int byte = bit / 32;
+    int shiftBits = bit % 32;
+
+    mask = mask << shiftBits;
+    vec[byte] = vec[byte] | mask;
+}
+
+
+int BoolVec::weight() 
+{
+    int weight = 0;
+    for (size_t i = 0; i < bits; i++) 
+    {
+        unsigned int mask = 1 << (i % 32);
+        if (vec[i / 32] & mask) {
+            weight++;
+        }
+    }
+    return weight;
+}
+
+BoolVec BoolVec::operator~ () 
+{
+    BoolVec result(bits);
+
+    unsigned int mask = 0xffffffff;
+
+    for (size_t i = 0; i < sizeVector; i++) {
+        result.vec[i] = vec[i] ^ mask;
+    }
+
+    int remainingBits = bits % 32;
+    if (remainingBits > 0) {
+        mask = (1 << remainingBits) - 1;
+        result.vec[sizeVector - 1] &= mask;
+    }
+
+    return result;
+}
+
+
+BoolVec BoolVec::operator& (const BoolVec& robj) 
+{
+    BoolVec result(bits);
+
+    if (vec == NULL || robj.vec == NULL ||
+        bits != robj.bits) {
+        return result;
+    }
+
+    for (int i = 0; i < result.sizeVector; i++)
+        result.vec[i] = vec[i] & robj.vec[i];
+
+    return result;
+}
+
+
+BoolVec BoolVec::operator| (const BoolVec& robj) 
+{
+    BoolVec result(bits);
+
+    if (vec == NULL || robj.vec == NULL || bits != robj.bits) 
+    {
+        return result;
+    }
+
+    for (int i = 0; i < result.sizeVector; i++)
+        result.vec[i] = vec[i] | robj.vec[i];
+
+    return result;
+}
+
+
+ostream& operator<< (ostream& out, const BoolVec& vec) 
+{
+    out << vec.ConvertVecToStr();
+    return out;
+}
+
+
+istream& operator>> (istream& in, BoolVec& vec) 
+{
+    string str;
+    cout << "Input vector: ";
+    in >> str;
+    BoolVec result(str.c_str());
+
+    vec = result;
+
+    return in;
+}
+
+
+class BoolMatrix {
+private:
+    BoolVec* boolMatrix;
+    size_t rows, cols;
 public:
-    BoolM(int k = 1, int l = 1) : m(k), n(l)
-    {
-        if(k <= 0 || l <= 0)
-        {
-            cout << "Error index" << endl;
-            exit(-4);
-        }
+    BoolMatrix(size_t n = 1, size_t m = 1);
+    BoolMatrix(const BoolMatrix&);
+    ~BoolMatrix();
+    BoolVec& operator[] (size_t);
+    BoolMatrix operator= (const BoolMatrix&);
+    void TopSort(BoolMatrix&, size_t, vector<int>&);
+    friend ostream& operator<< (ostream&, BoolMatrix&);
 
-        bm = new BoolV[m];
-        for(int i = 0; i < m; i++)
-        {
-            bm[i] = BoolV(n);
-        }
-    }
-
-    BoolM(const BoolM &other) : m(other.m), n(other.n)
-    {
-        bm = new BoolV[m];
-        for(int i = 0; i < m; i++)
-        {
-            bm[i] = other.bm[i];
-        }
-    }
-
-    ~BoolM()
-    {
-        delete[] bm;
-    }
-
-    int getRows()//строчки
-    {
-        return m;
-    }
-
-    int getCols()//cтолбцы
-    {
-        return n;
-    }
-
-    BoolV & operator[] (int pos)
-    {
-        return bm[pos];
-    }
-
-    BoolM operator= (const BoolM &other)
-    {
-        if(this == &other)
-            return *this;
-
-        delete[] bm;
-        m = other.m;
-        n = other.n;
-        bm = new BoolV[m];
-        for(int i = 0; i < m; i++)
-        {
-            bm[i] = other.bm[i];
-        }
-        return *this;
-    }
-
-    void TopSort(BoolM &mx, int m, vector<int> &a)
-    {
+    size_t getRows() { return rows; }
+    size_t getCols() { return cols; }
 
 
-
-        BoolV vec0(m), vec1(m), vec2(m), mask(m);
-
-        while(vec0.weight() != m)
-        {
-            for(int i = 0; i < m; i++)
-                    cout << mx;
-
-            vec1 = mask;
-
-            for(int i = 0; i < m; i++)
-                vec1 = vec1 | mx[i];
-
-            vec1 = ~vec1;//вершины в которые нельзя попасть;
-
-            vec2 = vec1 & (~vec0);//вершины в которых ещё не побывали
-
-            if(vec2 == mask)
-            {
-                cout << "dsafjkasjf" << endl;
-                exit(-5);
-            }
-
-            for(int i = 0; i < m; i++)
-            {
-                if(vec2[i] == 1)
-                {
-                    a.push_back(i + 1);
-                    mx[i] = mask;
-                }
-            }
-
-            vec0 = vec0 | vec2;
-
-            cout << "Vec0 - " << vec0 << endl;
-            cout << "Weight - " << vec0.weight() << endl;
-
-        }
-
-    }
-
-    BoolM CreateMxByGraf()
-    {
-        string path = "graf.txt";
-        //ifstream file("graf.txt");
-        ifstream file;
-        file.open(path);
-
-        if(!file.is_open())
-        {
-            cout << "file not open" << endl;
-            exit(-4);
-        }
-
-        int a, b;//a - начало b - конец 
-        int m;//количество вершин
-
-        file >> m;
-        BoolM Mx(m, m);
-
-        while(file >> a >> b)
-        {
-            Mx[a - 1].set1(b - 1);
-        }
-
-        file.close();
-
-        return Mx;
-    }
-
-    friend ostream& operator<< (ostream& os, BoolM &boolM)
-    {
-        for(int i = 0; i < boolM.m; i++)
-        {
-            os << boolM[i] << endl;
-        }
-        return os;
-    }
 };
 
-int main()
+
+void BoolMatrix::TopSort(BoolMatrix& matrix, size_t m, vector<int>& arr) 
 {
-    // setlocale(LC_ALL, "ru");
+    BoolVec vec0(m);  // Использованные вершины
+    BoolVec vec1(m);  // Нет пути
+    BoolVec vec2(m);  // Ответ
+    BoolVec mask(m); 
 
-    BoolM mx;   
-
-    mx.CreateMxByGraf();
-
-    cout << mx << endl;
-
-    vector<int> a;
-
-    mx.TopSort(mx, mx.getRows(), a);
-
-    for(int p : a)
+    while (vec0.weight() != m) 
     {
-        cout << p << " ";
+        cout << matrix << endl;
+        vec1 = mask;
+
+        // Вершины в которые можно попасть
+        for (int i = 0; i < m; i++)
+            vec1 = vec1 | matrix[i];
+
+        // Вершины, в которые нельзя попасть через другие
+        vec1 = ~vec1;
+
+        cout << "vec1 = " << vec1 << endl;
+
+        // Вершины, которые не посетили
+        vec2 = vec1 & (~vec0);
+        cout << vec2 << endl;
+
+        if (vec2 == mask) 
+        {
+            cout << "Cycle!" << endl;
+            exit(1);
+        }
+
+        for (int i = 0; i < m; i++) 
+        {
+            if (vec2[i] == 1) {
+                arr.push_back(i + 1);
+                matrix[i] = mask;   // Удаляем рёбра из найденных вершин
+            }
+        }
+
+        vec0 = vec0 | vec2;
+
+        cout << "Vec0 = " << vec0 << endl;
+        cout << "Weight = " << vec0.weight() << endl;
     }
+}
+
+
+BoolMatrix ReadFromFile() 
+{
+    ifstream fileIn("graf.txt");
+    if (!fileIn.is_open()) 
+    {
+        cout << "File can't find\n";
+        exit(1);
+    }
+
+    int a, b;
+    int m;
+
+    fileIn >> m;
+    BoolMatrix boolM(m, m);
+    while (!fileIn.eof()) 
+    {
+        fileIn >> a;
+        fileIn >> b;
+        boolM[a - 1].Set1(b - 1);
+    }
+    fileIn.close();
+
+    return boolM;
+}
+
+
+BoolMatrix::BoolMatrix(size_t n, size_t m) 
+{
+    if (!n || !m) {
+        boolMatrix = NULL;
+        rows = 0;
+        cols = 0;
+        return;
+    }
+
+    rows = n;
+    cols = m;
+    boolMatrix = new BoolVec[rows];
+
+    for (int i = 0; i < rows; i++)
+        boolMatrix[i] = BoolVec(cols);
+}
+
+
+BoolMatrix::BoolMatrix(const BoolMatrix& obj) 
+{
+    rows = obj.rows;
+    cols = obj.cols;
+    boolMatrix = new BoolVec[rows];
+    for (int i = 0; i < rows; i++)
+        boolMatrix[i] = obj.boolMatrix[i];
+}
+
+
+BoolMatrix::~BoolMatrix() 
+{
+    if (boolMatrix)
+        delete[] boolMatrix;
+    rows = 0;
+    cols = 0;
+}
+
+BoolMatrix BoolMatrix::operator= (const BoolMatrix& obj) 
+{
+    if (this == &obj)
+        return *this;
+
+    delete[] boolMatrix;
+
+    rows = obj.rows;
+    cols = obj.cols;
+
+    boolMatrix = new BoolVec[rows];
+
+    for (int i = 0; i < rows; i++)
+        boolMatrix[i] = obj.boolMatrix[i];
+
+    return *this;
+}
+
+
+ostream& operator<< (ostream& out, BoolMatrix& obj) 
+{
+    for (int i = 0; i < obj.rows; i++)
+        out << obj.boolMatrix[i] << endl;
+
+    return out;
+}
+
+
+BoolVec& BoolMatrix::operator[](size_t index) 
+{
+    if (index >= rows) 
+    {
+        cerr << "Error []: index out of range!\n";
+        exit(1);
+    }
+    return boolMatrix[index];
+}
+
+
+int main() 
+{
+    BoolMatrix matrix;
+
+    matrix = ReadFromFile();
+    cout << matrix << endl;
+    
+    vector<int> arr;
+    
+    matrix.TopSort(matrix, matrix.getRows(), arr);
+
+    for (auto p : arr)
+        cout << p << " ";
+
     cout << endl;
 
     return 0;
